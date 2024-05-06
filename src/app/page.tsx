8cd81c973baea26bb64c11fded0bed61c9fca0a0
LukySand <luky.sand@gmail.com>
2024-05-06 15:56:06 -0300

    deleted unecessary logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,6 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center relative">
       <Placer />
-      {/* <Image
-        src="/Map_Classic_Scenery.jpg"
-        alt="clash_map"
-        width={2000}
-        height={500}
-        className="inset-0 w-full h-full object-cover"
-      /> */}
     </main>
   );
 }
