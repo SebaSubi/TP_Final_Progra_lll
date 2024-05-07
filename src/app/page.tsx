2bee34705bba1361dff177f96670e12191393f1e
123Damian <damian789frick@gmail.com>
2024-05-07 15:23:26 -0300

    function that changes the image after a time interval

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       
       return () => clearInterval(intervalId);
   }, [])
-  
+  /**/
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
