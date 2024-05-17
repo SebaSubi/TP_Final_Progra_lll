be2c79e9dcc703bb9dbc467b05c6bd95e7e2376a
eve <eveernst77@gmail.com>
2024-05-16 23:25:46 -0300

    mapa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 // import GoldMine from "./components/GoldMine";
 import SideNav from "./construccion/mode/constructionBar";
+import Mapa from "./components/mapa";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Mapa />
       {/* <SideNav />
       <GoldMine /> */}
       {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
