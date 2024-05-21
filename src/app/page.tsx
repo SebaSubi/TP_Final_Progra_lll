ab5dd573bd296208b1796135d167679a86d5373d
eve <eveernst77@gmail.com>
2024-05-21 20:58:28 -0300

    mapa interactivo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+'use client';
+
 import Image from "next/image";
 // import GoldMine from "./components/GoldMine";
-import SideNav from "./construccion/mode/constructionBar";
+// import SideNav from "./construccion/mode/constructionBar";
 import Mapa from "./components/mapa";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Mapa />
+      <body>
+        {/* Llamada a la función que contiene el mapa */}
+        <Mapa />
+      </body>
       {/* <SideNav />
       <GoldMine /> */}
       {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
