96128ab49f30fc824903bd7cb838ea94254917c6
LukySand <luky.sand@gmail.com>
2024-06-20 20:43:28 -0300

    added damian changes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,51 +1,50 @@
-import CredentialsProvider from "next-auth/providers/credentials"
-import NextAuth from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth";
 import { connect } from "@/app/libs/mongodb";
 import User from "@/app/models/user";
 import bcrypt from "bcryptjs";
 
-
-
 const handler = NextAuth({
-      providers: [
-        CredentialsProvider({
+  providers: [
+    CredentialsProvider({
       name: 'credentials',
       credentials: {
-        email: { label: "Email", type: "email", placeholder: "jsmith"},
-        password: {  label: "Password", type: "password", placeholder: "*******" },
-          },
-          async authorize(credentials, req) {
-            await connect();
-            const userFound = await User.findOne({email: credentials?.email, }).select("+password");
-            
-            console.log('User found:', userFound);
-          
-            if(!userFound) throw new Error("Invalid credentials");
-            const passwordMatch = await bcrypt.compare(credentials!.password, userFound.password)
-          
-            console.log('Password match:', passwordMatch);
-          
-            if (!passwordMatch) throw new Error("Invalid credentials");
-          
-            return userFound;
-          },
-      }),
-    ],
-    callbacks:{
-      jwt({account, token, user, profile, session}){
-        if(user) token.user = user;
-        return token;
+        email: { label: "Email", type: "email", placeholder: "jsmith" },
+        password: { label: "Password", type: "password", placeholder: "*" },
       },
-      session({session, token}) {
-        session.user = token.user as any;
-        console.log(session, token)
-        return session;
+      async authorize(credentials, req) {
+        await connect();
+        const userFound = await User.findOne({ email: credentials?.email }).select("+password");
+
+        console.log('User found:', userFound);
+
+        if (!userFound) throw new Error("Invalid credentials");
+        const passwordMatch = await bcrypt.compare(credentials!.password, userFound.password);
+
+        console.log('Password match:', passwordMatch);
+
+        if (!passwordMatch) throw new Error("Invalid credentials");
+
+        return userFound;
       },
+    }),
+  ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.user = user;
+      }
+      return token;
     },
-    pages: {
-      signIn: "/login",
-    }
+    session({ session, token }) {
+      session.user = token.user as any;
+      console.log(session, token);
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
 });
 
-
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
