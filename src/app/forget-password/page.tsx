9cec648fcab7133ee3d1db79d347de9881cca686
LukySand <luky.sand@gmail.com>
2024-06-23 17:08:37 -0300

    commented a line

--- src/app/forget-password/page.tsx
+++ src/app/forget-password/page.tsx
@@ -1,10 +1,9 @@
-"use client"
-import axios from 'axios';
-import { FormEvent, useState } from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import axios from "axios";
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
 
 function ForgetPasswordPage() {
-
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const router = useRouter();
@@ -13,7 +12,7 @@ function ForgetPasswordPage() {
     e.preventDefault();
 
     try {
-      const res = await axios.post('/api/auth/forget-password', { email });
+      const res = await axios.post("/api/auth/forget-password", { email });
 
       if (res.status === 200) {
         setMessage("Check your email for a link to reset your password.");
@@ -23,35 +22,62 @@ function ForgetPasswordPage() {
     } catch (error) {
       setMessage("An error occurred.");
     }
-  }
+  };
 
   const handleRegisterClick = (e: React.MouseEvent) => {
     e.preventDefault();
-    router.push('/');
-  }
+    router.push("/");
+  };
 
   return (
     <main className="container mx-auto flex flex-col justify-center items-center min-h-screen bg-black">
-      <img src="/p11chad.svg" alt="p11" className='mb-5' style={{ width: '15%', height: 'auto' }}/>
-      <h2 style={{textShadow: '3px 3px 2px rgba(255, 0, 0, 0.5)'}} className="text-5xl font-bold mb-6 text-center w-full text-red-500 mr-5"> SOS BLDO ?? </h2>
-      <h2 style={{textShadow: '3px 3px 2px rgba(255, 0, 0, 0.5)'}} className="text-2xl font-bold mb-6 text-center w-full text-red-500 mr-5"> COMO TE LA VAS A OLVIDAR GIL </h2>
-      <div className='justify-center flex items-center'>
-        <form onSubmit={handleSubmit} className="flex flex-col items-center p-4 border border-white rounded-lg">
-          <h2 style={{textShadow: '3px 3px 2px rgba(255, 0, 0, 0.5)'}} className="text-2xl font-bold mb-6 text-center w-full text-red-500 mr-5"> Reset your Password </h2>
-            <input 
-              type="Email" 
-              placeholder="Email" 
-              name="email" 
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded-lg mb-4 text-white bg-black"
-            />
-            <button className="text-1xl text-center text-white p-1 border border-white rounded-lg hover:bg-gray-900 mb-4 mr-2">Submit</button>
+      <img
+        src="/p11chad.svg"
+        alt="p11"
+        className="mb-5"
+        style={{ width: "15%", height: "auto" }}
+      />
+      <h2
+        style={{ textShadow: "3px 3px 2px rgba(255, 0, 0, 0.5)" }}
+        className="text-2xl font-bold mb-6 text-center w-full text-red-500 mr-5"
+      >
+        {" "}
+        COMO TE LA VAS A OLVIDAR GIL{" "}
+      </h2>
+      <div className="justify-center flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center p-4 border border-white rounded-lg"
+        >
+          <h2
+            style={{ textShadow: "3px 3px 2px rgba(255, 0, 0, 0.5)" }}
+            className="text-2xl font-bold mb-6 text-center w-full text-red-500 mr-5"
+          >
+            {" "}
+            Reset your Password{" "}
+          </h2>
+          <input
+            type="Email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded-lg mb-4 text-white bg-black"
+          />
+          <button className="text-1xl text-center text-white p-1 border border-white rounded-lg hover:bg-gray-900 mb-4 mr-2">
+            Submit
+          </button>
 
-            <p className="text-1xl text-center text-white">
-              <a href="/register" onClick={handleRegisterClick} className="underline hover:text-red-900">Go Back</a>
-            </p>
-            {message && <p>{message}</p>}
+          <p className="text-1xl text-center text-white">
+            <a
+              href="/register"
+              onClick={handleRegisterClick}
+              className="underline hover:text-red-900"
+            >
+              Go Back
+            </a>
+          </p>
+          {message && <p>{message}</p>}
         </form>
       </div>
     </main>
